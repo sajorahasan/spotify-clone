15889851bbc744ab6e756a5bf63e1641a14b78ba
Hasan Sajora <hasan.sajora@blattnertech.com>
2024-09-24 12:06:35 +0530

    Added GestureHandlerRootView provider and reanimated babel config

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,42 @@
 import { Slot } from 'expo-router';
+import { useCallback, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+function RootProviders({ children }: { children: React.ReactNode }) {
+  return <GestureHandlerRootView style={styles.container}>{children}</GestureHandlerRootView>;
+}
+
+function RootLayoutNav() {
+  // const [loaded] = useFonts({
+  //   'WorkSans-Regular': require('../../assets/fonts/WorkSans-Regular.ttf'),
+  //   'WorkSans-SemiBold': require('../../assets/fonts/WorkSans-SemiBold.ttf'),
+  //   'Overpass-SemiBold': require('../../assets/fonts/Overpass-SemiBold.ttf'),
+  // });
+
+  // const hideSplash = useCallback(async () => {
+  //   await SplashScreen.hideAsync();
+  // }, []);
+
+  // useEffect(() => {
+  //   if (loaded) {
+  //     setTimeout(() => hideSplash(), 1500);
+  //   }
+  // }, [hideSplash, loaded]);
+
+  return (
+    <RootProviders>
+      <Slot />
+    </RootProviders>
+  );
+}
 
 export default function RootLayout() {
-  return <Slot />;
+  return <RootLayoutNav />;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
